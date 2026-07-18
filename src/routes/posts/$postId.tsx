d80207ecb4eba6eb6ec1397a8452b06f98e66282
MUSAB ALQARNI <78758932+MusabSwe@users.noreply.github.com>
2026-07-18 18:39:08 +0300

    Dynamic + Loaders

--- src/routes/posts/$postId.tsx
+++ src/routes/posts/$postId.tsx
@@ -1,5 +1,5 @@
-import { useQuery } from '@tanstack/react-query'
-import { createFileRoute, useParams } from '@tanstack/react-router'
+// import { useQuery } from '@tanstack/react-query'
+import { createFileRoute, useLoaderData } from '@tanstack/react-router'
 
 const fetchPost = async (postId: string) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
@@ -8,20 +8,18 @@ const fetchPost = async (postId: string) => {
 }
 
 export const Route = createFileRoute('/posts/$postId')({
-    params: {
-        parse: (params) => ({
-            postId: String(params.postId),
-        }),
-    },
     component: RouteComponent,
+    loader: ({ params }) => fetchPost(params.postId) // I can pass more than params such as deps, 
 })
 
 function RouteComponent() {
-    const { postId } = useParams({ from: '/posts/$postId' }); // useParams is a hook from @tanstack/react-router that allows you to access the parameters of the current route. In this case, it retrieves the postId parameter from the URL.
-    const { data } = useQuery({
-        queryKey: ['post', postId], // The query key is an array that uniquely identifies the query. In this case, it consists of the string 'post' and the postId parameter. This allows React Query to cache and manage the data for each post separately.
-        queryFn: () => fetchPost(postId), // The query function is a function that fetches the data for the query. In this case, it calls the fetchPost function with the postId parameter.
-    })
+    const data = useLoaderData({ from: '/posts/$postId' });
+   
+    // const { postId } = useParams({ from: '/posts/$postId' }); // useParams is a hook from @tanstack/react-router that allows you to access the parameters of the current route. In this case, it retrieves the postId parameter from the URL.
+    // const { data } = useQuery({
+    //     queryKey: ['post', postId], // The query key is an array that uniquely identifies the query. In this case, it consists of the string 'post' and the postId parameter. This allows React Query to cache and manage the data for each post separately.
+    //     queryFn: () => fetchPost(postId), // The query function is a function that fetches the data for the query. In this case, it calls the fetchPost function with the postId parameter.
+    // })
     return (
         <div>
             {JSON.stringify(data)}
